b671cf3846bf18f6c8ddbbffc5777ebba6ae8229
mtouthang <huebart16@gmail.com>
2023-05-03 10:11:40 +0530

    work structure component added

---
+++ client/src/components/Work.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <div>
+      <h2> 02 Where I've Worked </h2> <div></div>
+      <div>
+        <div></div>
+        <div className="btn-work ">
+          <button> Apple </button>
+          <button> Google</button>
+          <button> Microsoft </button>
+        </div>
+        <div>
+          <h3> Work title</h3>
+          <p> date - 2023</p>
+          <ul>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
+              repudiandae a praesentium saepe provident sed facilis perferendis
+              soluta inventore veniam.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
+              repudiandae a praesentium saepe provident sed facilis perferendis
+              soluta inventore veniam.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
+              repudiandae a praesentium saepe provident sed facilis perferendis
+              soluta inventore veniam.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
